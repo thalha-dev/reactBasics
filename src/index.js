549729e000bdd5ad65fe1208df5473557690f060
thalha-dev <dev.thalha@gmail.com>
2023-02-28 16:53:49 +0530

    redux two complete

--- src/index.js
+++ src/index.js
@@ -1,44 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ReduxApp2 from "./redux/two/ReduxApp2";
+import "./redux/two/index.css";
+import { store } from "./redux/two/app/store";
+import { Provider } from "react-redux";
+import { fetchUsers } from "./redux/two/features/users/usersSlice";
 
-/* import RouterApp from "./reactRouter/routerApp"; */
-/* import "./reactRouter/index.css"; */
-
-/* import ContextApp from "./context/ContextApp"; */
-
-/* import StateApp from "./useStateAnduseReducer/StateApp"; */
-
-/* import EffectApp from "./useEffect/EffectApp"; */
-
-/* import ModalApp from "./modal/one/ModalApp"; */
-/* import ModalApp2 from "./modal/two/ModalApp2"; */
-
-/* import ReduxApp from "./redux/one/ReduxApp"; */
-/* import { store } from "./redux/one/app/store"; */
-/* import "./redux/one/index.css"; */
-
-/* import ReduxApp2 from "./redux/two/ReduxApp2"; */
-/* import "./redux/two/index.css"; */
-/* import { store } from "./redux/two/app/store"; */
-/**/
-/* import { Provider } from "react-redux"; */
-
-/* import AxiosApp from "./axios/one/AxiosApp"; */
-import AxiosApp2 from "./axios/two/AxiosApp2";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <AxiosApp2 />
-  </React.StrictMode>
-);
-
-/* <Provider store={store}> */
-/*   <ReduxApp2 /> */
-/* </Provider> */
+store.dispatch(fetchUsers());
 
 /* <React.StrictMode> */
-/*   <ContextApp /> */
-/*   <StateApp /> */
-/*   <RouterApp /> */
 /* </React.StrictMode> */
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <ReduxApp2 />
+  </Provider>
+);
