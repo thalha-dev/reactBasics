9e7815f71a884859cab1cba57906f982ab551b3b
thalha-dev <dev.thalha@gmail.com>
2023-03-06 19:02:03 +0530

    redux three

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import EffectApp2 from "./useEffect/two/EffectApp2";
-/* import ReduxApp2 from "./redux/two/ReduxApp2"; */
-/* import "./redux/two/index.css"; */
-/* import { store } from "./redux/two/app/store"; */
-/* import { Provider } from "react-redux"; */
-/* import { fetchUsers } from "./redux/two/features/users/usersSlice"; */
+import ReactDOM from "react-dom";
+import "./redux/three/src/index.css";
+import App from "./redux/three/src/App";
+import store from "./redux/three/src/app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./redux/three/src/serviceWorker";
 
-/* store.dispatch(fetchUsers()); */
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
-/* <React.StrictMode> */
-/* </React.StrictMode> */
-
-/* <Provider store={store}> */
-/*   <ReduxApp2 /> */
-/* </Provider> */
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<EffectApp2 />);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/redux/three/ReduxApp3.js
@@ -0,0 +1,9 @@
+function ReduxApp3() {
+  return (
+    <main className="App">
+      <h1>ReduxApp3</h1>
+    </main>
+  );
+}
+
+export default ReduxApp3;
